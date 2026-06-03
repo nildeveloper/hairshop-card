f500ff07727db684edc5cfbf8e0bf65c7c81f6e8
liuhaolu.lhl <liuhaolu.lhl@alibaba-inc.com>
2026-06-03 17:25:23 +0800

    init: 理发店次卡管理 PWA

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'haircard-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './sw.js', './icon.svg'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // cache-first: 离线优先读缓存
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});

a0d7468ef77c8914766c52a476cede88bdf5eccf
liuhaolu.lhl <liuhaolu.lhl@alibaba-inc.com>
2026-06-04 15:08:10 +0800

    build(sw.js, index.html): 更新缓存版本号并修正Turnstile站点密钥配置
    本次更新调整了服务工作线程中的缓存标识符至v23版本，并恢复了Cloudflare Turnstile的人机验证站点密钥设置。

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'haircard-v22';
+const CACHE = 'haircard-v23';
 const ASSETS = ['./', './index.html', './manifest.json', './sw.js', './icon.svg'];
 
 self.addEventListener('install', e => {

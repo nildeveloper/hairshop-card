4860b1d223505333438127e4e2f3a0e079f8e16e
liuhaolu.lhl <liuhaolu.lhl@alibaba-inc.com>
2026-06-04 16:06:29 +0800

    fix(sw.js, index.html): 更新缓存版本并优化 Turnstile 加载机制
    更新 Service Worker 缓存版本号至 v25，并改进 Turnstile 验证脚本的加载方式，采用静态预加载结合固定容器的方式，提高兼容性并解决潜在的脚本执行问题。

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'haircard-v27';
+const CACHE = 'haircard-v28';
 const ASSETS = ['./', './index.html', './manifest.json', './sw.js', './icon.svg'];
 
 self.addEventListener('install', e => {
